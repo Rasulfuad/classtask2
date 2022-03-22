eb34339de462d249eda6de9b680e3ff6dbb006c3
Rasulfuad <fuadar@code.edu.az>
2022-03-22 21:09:56 +0400

    feat : add classt task 2

---
+++ class task2.encpsltn/Classtask2.Encapsulation/Classtask2.Encapsulation/Models/User.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Classtask2.Encapsulation.Models
+{
+    internal class User
+    {
+        public string Username;
+
+        private int _Age;
+        public int Age
+        {
+            get { return _Age; }
+            set
+            {
+                if (value > 0)
+                {
+                    Console.WriteLine("age 0-dan boyuk olmalidir");
+                    return;
+                }
+                _Age = value;
+                Console.WriteLine("age set olundu");
+            }
+        }
+
+        private string _Password;
+        public string Password
+        {
+            get { return _Password; }
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) && value.Length > 0 && PasUp(value) == true && PasInt(value))
+
+                {
+                    _Password = value;
+                }
+            }
+
+        }
+
+
+        private bool PasUp(string value)
+        {
+
+
+            char[] meth1 = value.ToCharArray();
+            for (int i = 0; i < meth1.Length; i++)
+                if (char.IsUpper(meth1[i]) == true)
+
+                {
+
+                    Console.WriteLine("en az 1 herf olmalidir");
+                }
+            return true;
+
+        }
+
+        private bool PasInt(string Password)
+        {
+
+
+            char[] meth2 = Password.ToCharArray();
+            for (int i = 0; i < meth2.Length; i++)
+                if (char.IsNumber(meth2[i]) == true)
+                {
+                    Console.WriteLine("en az 1 reqem olmalidir");
+                }
+
+
+            return true;
+
+
+        }
+    }
+}
---
+++ class task2.encpsltn/Classtask2.Encapsulation/Classtask2.Encapsulation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Classtask2.Encapsulation.Models;
+
+namespace Classtask2.Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User users = new User();
+
+            Console.Write("Username daxil edin: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Sifre daxil edin: ");
+            string password = Console.ReadLine();
+
+            Console.Write("Yasinizi daxil edin: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
